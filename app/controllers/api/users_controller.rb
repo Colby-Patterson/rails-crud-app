593969f49be8f5b32878e57e88c7d5c0ca558da0
Colby Patterson <cpatt@byui.edu>
2022-06-06 15:29:27 -0600

    Post/Create working

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,4 +7,19 @@ class Api::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user
   end
+
+  def create
+    user = User.new(user_params)
+    if(user.save)
+      render json: user
+    else
+      puts "Error"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
     get '/users', to: 'users#index'
     get '/users/:id', to: 'users#show'
+
+    post '/users', to: 'users#create'
   end
 end
