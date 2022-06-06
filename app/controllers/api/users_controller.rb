8b2c708b27a4afc8d92399f363a281c8f4f15f2d
Colby Patterson <cpatt@byui.edu>
2022-06-06 15:00:22 -0600

    Get show working

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,4 +2,9 @@ class Api::UsersController < ApplicationController
   def index
     render json: User.all
   end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
 
     get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
   end
 end
