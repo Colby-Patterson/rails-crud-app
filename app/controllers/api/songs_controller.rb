8f4764e94ed28f5a9e92c02bea6dbbeed10ea903
Colby Patterson <cpatt@byui.edu>
2022-06-06 17:00:24 -0600

    Added update to song controller

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,4 +1,5 @@
 class Api::SongsController < ApplicationController
+  before_action :set_song, only: [:update]
   def index
     render json: Song.all
   end
@@ -8,9 +9,34 @@ class Api::SongsController < ApplicationController
     render json: song
   end
 
+  def create
+    song = Song.new(song_params)
+    if(song.save)
+      render json: song
+    end
+  end
+
+  def update
+    if @song.update(song_params)
+      render json: @song
+    else
+      # render json: {errors: item.errors.full_messages}, status 422
+    end
+  end
+
   def destroy
     song = Song.find(params[:id])
     song.destroy
     render json: {message: 'Song deleted'}
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:song_name, :artist)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,11 @@ Rails.application.routes.draw do
     #Song routes
     get '/songs', to: 'songs#index'
     get '/songs/:id', to: 'songs#show'
+    
+    post '/songs', to: 'songs#create'
 
+    put '/songs/:id', to: 'songs#update'
+    
     delete '/songs/:id', to: 'songs#destroy'
   end
 end
